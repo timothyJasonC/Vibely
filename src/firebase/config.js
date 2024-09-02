62c5eaf25802c7f07c705a8eeddf79c6a24ffb88
Timothy Jason <timothycaviezel@gmail.com>
2024-09-02 09:49:43 +0700

    fix firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,16 +3,15 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtuLKCjKF0ETMGb-qLndX_UgaanmTw56k",
-  authDomain: "vibely-92262.firebaseapp.com",
-  projectId: "vibely-92262",
-  storageBucket: "vibely-92262.appspot.com",
-  messagingSenderId: "10664900557",
-  appId: "1:10664900557:web:133a1644a6abf82b41b684",
-  measurementId: "G-B866P4QE11"
+  apiKey: process.env.API_KEY ?? '',
+  authDomain: process.env.AUTH_DOMAIN ?? '',
+  projectId: process.env.PROJECT_ID ?? '',
+  storageBucket: process.env.STORAGE_BUCKET ?? '',
+  messagingSenderId: process.env.MESSAGING_SENDER_ID ?? '',
+  appId: process.env.APP_ID ?? '',
+  measurementId: process.env.MEASUREMENT_ID ?? ''
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
